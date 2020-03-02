b3a8183457873714331b8ce2261917084ffdaa58
karsv <Pbvf2019!>
2020-03-02 10:23:37 +0200

    add custom exception

--- src/main/java/com/task/amazon/utils/impl/FileReaderService.java
+++ src/main/java/com/task/amazon/utils/impl/FileReaderService.java
@@ -1,11 +1,11 @@
 package com.task.amazon.utils.impl;
 
+import com.task.amazon.exceptions.DataProcessingException;
 import com.task.amazon.utils.ReaderService;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -18,8 +18,7 @@ public class FileReaderService implements ReaderService {
         try {
             return Files.readAllLines(Path.of(path));
         } catch (IOException e) {
-            e.printStackTrace();
-            return new ArrayList<String>();
+            throw new DataProcessingException("Can't read file!", e);
         }
     }
 }
