37fe8c9187c11e84164b23f8b863f27e995abb80
karsv <Pbvf2019!>
2020-03-03 14:46:54 +0200

    try commit +1+1

--- src/main/java/com/task/amazon/controllers/InitController.java
+++ src/main/java/com/task/amazon/controllers/InitController.java
@@ -23,7 +23,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class InitController {
-    private static final String FILE_NAME = "/src/main/resources/data.csv";
+    private static final String FILE_NAME = "src/main/resources/data.csv";
 
     private final CsvParserService csvParserService;
 
