3e8be72890f97ad14931cbb6a9a69f1d8940236d
karsv <Pbvf2019!>
2020-03-14 13:44:58 +0200

    delete auth_whitelist

--- src/main/java/com/task/amazon/entities/Role.java
+++ src/main/java/com/task/amazon/entities/Role.java
@@ -5,10 +5,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import javax.persistence.Table;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name = "roles")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
